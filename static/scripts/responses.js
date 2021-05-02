b30eeaa5e1b6184ded7e927c25af9d669be3fbaf
bishal parida <paridabishal982@gmail.com>
2021-05-02 10:13:37 -0500

    np

---
+++ static/scripts/responses.js
@@ -0,0 +1,20 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "Admission" ,"admission") {
+        return( window.location="file:///C:/Users/oiu/Desktop/debosati/collage%20manage%20ment%20system/Admission.html")  ;
+       
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
